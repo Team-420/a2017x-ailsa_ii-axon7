b55e65c962983f52c8c5fcf8fe7a8792f0ae28de
S133PY <thiviyan@gmail.com>
2020-09-04 12:48:45 +0530

    our buil script

--- build.sh
+++ build.sh
@@ -1,26 +1,24 @@
-export KBUILD_BUILD_USER=baddar90
+export KBUILD_BUILD_USER=S133PY
 export ARCH=arm64
-export CROSS_COMPILE=$HOME/arm64-gcc-10/bin/aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=$HOME/arm32-gcc-9.1/bin/arm-eabi-
+#export CROSS_COMPILE=/mnt/kernels/caf_kernel_zte_msm8996/toolchains/gcc10/bin/aarch64-linux-gnu-
+#export CROSS_COMPILE=/home/sleepy/android/toolchains/GCC-10/bin/aarch64-linux-elf-
+export CROSS_COMPILE=/home/sleepy/Lineage-GCC/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=/home/sleepy/Lineage-GCC/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+#export CROSS_COMPILE_ARM32=/mnt/kernels/caf_kernel_zte_msm8996/toolchains/arm32-gcc/bin/arm-eabi-
 
-BUILD="/home/baddar/build"
-OUT="/home/baddar/out"
+
+BUILD="/mnt/kernels/caf_kernel_zte_msm8996/build"
+OUT="/mnt/kernels/caf_kernel_zte_msm8996/out"
 #NPR=`expr $(nproc) + 1`
 
-echo "cleaning build..."
-if [ -d "$BUILD" ]; then
-rm -rf "$BUILD"
-fi
-if [ -d "$OUT" ]; then
-rm -rf "$OUT"
-fi
+
 
 echo "setting up build..."
 mkdir "$BUILD"
-make O="$BUILD" lineageos_axon7_defconfig
+make O="$BUILD" nethunter_defconfig
 
 echo "building kernel..."
-make O="$BUILD" -j30
+make O="$BUILD" -j64
 
 echo "building modules..."
 make O="$BUILD" INSTALL_MOD_PATH="." INSTALL_MOD_STRIP=1 modules_install
