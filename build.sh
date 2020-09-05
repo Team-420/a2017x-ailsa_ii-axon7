2d051d2febe347d68556e9303b6da012e9e89fe0
S133PY <thiviyan@gmail.com>
2020-09-05 17:25:29 +0530

    small changes in build script
    added command to freshly build the kernel..

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ export CROSS_COMPILE=/home/sleepy/android/toolchains/GCC-10/bin/aarch64-linux-el
 #export CROSS_COMPILE_ARM32=/home/sleepy/Lineage-GCC/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 export CROSS_COMPILE_ARM32=/mnt/kernels/caf_kernel_zte_msm8996/toolchains/arm32-gcc/bin/arm-eabi-
 
-
+rm -rf out
+rm -rf build
 BUILD="/mnt/kernels/caf_kernel_zte_msm8996/build"
 OUT="/mnt/kernels/caf_kernel_zte_msm8996/out"
 #NPR=`expr $(nproc) + 1`
@@ -26,7 +27,6 @@ make O="$BUILD" -j8 INSTALL_MOD_PATH=MODULES_OUT modules_install
 
 mkdir -p $OUT/modules
 mv "$BUILD/arch/arm64/boot/Image.gz-dtb" "$OUT/Image.gz-dtb"
-rm -rf $OUT
 mv "$BUILD/MODULES_OUT" "$OUT/modules"
 
 echo "Image.gz-dtb & modules can be found in $BUILD"
